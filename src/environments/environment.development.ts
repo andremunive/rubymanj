f70e91dc3ade49761308a8af327db7dc2da62d42
Andres Munive <fmunive@getontop.com>
2026-04-19 12:46:18 -0500

    Security

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://ejdunrshkgywzcakzxnj.supabase.co',
-  supabaseKey: 'sb_publishable_CswzouAojWXGwAph6zliQw_7g3O9w5o',
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  supabaseUrl: 'https://ejdunrshkgywzcakzxnj.supabase.co',
-  supabaseKey: 'sb_publishable_CswzouAojWXGwAph6zliQw_7g3O9w5o',
-};
